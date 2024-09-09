d170de337c9a8affb34faa77f67ee06d6633a436
martingreen-pe <martingreen.pe@gmail.com>
2024-09-09 13:40:31 -0500

    cambios en settins blog post netlify12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,20 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify'; // Reemplaza la importación anterior
 
 export default defineConfig({
   output: 'server',
   adapter: netlify({
-    functionPerRoute: true,
+    functionPerRoute: true,  // Configura el adaptador correctamente
     dist: new URL('./dist/', import.meta.url),
   }),
-  integrations: [
-    // Cualquier otra integración que estés usando
-  ],
-  // Habilita los scripts del lado del cliente
-  scripts: {
-    clientEntry: true
-  },  
   content: {
     collections: {
-    posts: {
-      schema: 'config:content',
-      directory: 'src/content/posts',
-     },
-    }
-  },   
+      posts: {
+        schema: 'config:content',
+        directory: 'src/content/posts',
+      },
+    },
+  },
   vite: {
     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg'],
   },     
